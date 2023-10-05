ba773b124b75b1d93960db242f5020958a69b0c3
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-10-05 20:16:51 +0100

    build a context for service impls

--- events_test.ts
+++ events_test.ts
@@ -1,31 +1,56 @@
-import {assertEquals} from "https://deno.land/std/assert/mod.ts"
-import {Effect, Context} from "npm:effect@^2.0.0-next.44"
-import {FxService, handleEventProgram} from "./events.ts";
+import { assertEquals } from "https://deno.land/std/assert/mod.ts"
+import { Effect, Context } from "npm:effect@^2.0.0-next.44"
+import { FxService, handleEventProgram } from "./events.ts";
 
 // this is a type-level id for the service - it must be typeswise unique
-export interface TestInputServiceA {readonly _: unique symbol}
+export interface TestInputServiceA { readonly _: unique symbol }
 // this is the service interface - it may have the same shape as another service (TestInputServiceBI)
 export interface TestInputServiceAI extends FxService<number> {
-    readonly fx: () => Effect.Effect<never, never, number>}
-export const TestInputServiceA = Context.Tag<TestInputServiceA,TestInputServiceAI>("TestInputServiceA")
+    readonly fx: () => Effect.Effect<never, never, number>
+}
+export const TestInputServiceA = Context.Tag<TestInputServiceA, TestInputServiceAI>("TestInputServiceA")
 
-export interface TestInputServiceB {readonly _: unique symbol}
+export interface TestInputServiceB { readonly _: unique symbol }
 export interface TestInputServiceBI extends FxService<number> {
-    readonly fx: () => Effect.Effect<never, never, number>}
-export const TestInputServiceB = Context.Tag<TestInputServiceB,TestInputServiceBI>("TestInputServiceB")
+    readonly fx: () => Effect.Effect<never, never, number>
+}
+export const TestInputServiceB = Context.Tag<TestInputServiceB, TestInputServiceBI>("TestInputServiceB")
 
-export interface TestOutputServiceC {readonly _: unique symbol}
-export interface TestOutputServiceCI extends FxService<[number,number]> {
-    readonly fx: (v?: [number,number]) => Effect.Effect<never, never, [number,number]>}
-export const TestOutputServiceC = Context.Tag<TestOutputServiceC,TestOutputServiceCI>("TestOutputServiceC")
+export interface TestOutputServiceC { readonly _: unique symbol }
+export interface TestOutputServiceCI extends FxService<[number, number]> {
+    readonly fx: (v?: [number, number]) => Effect.Effect<never, never, [number, number]>
+}
+export const TestOutputServiceC = Context.Tag<TestOutputServiceC, TestOutputServiceCI>("TestOutputServiceC")
 
-// a pure handler fn - the params are simple data and must match the 
+// bundle some service impls into a Context
+const context = Context.empty().pipe(
+    Context.add(TestInputServiceA, TestInputServiceA.of({
+        fx: () => Effect.succeed(10)
+    })),
+    Context.add(
+        TestInputServiceB,
+        TestInputServiceB.of({
+            fx: () => Effect.succeed(5)
+        })),
+    Context.add(
+        TestOutputServiceC,
+        TestOutputServiceC.of({
+            fx: (v?: [number, number]) => {
+                console.log("[number,number]", v)
+                return Effect.succeed(v === undefined ? [0, 0] : v)
+            }
+        })))
+
+// a pure handler fn - business logic goes here
+//
+// the params are simple data and must match the 
 // value types of the input Services, while the list of return values
 // are also simple data which must match the data param types of
 // the output Services
-const pureHandler = (a: number, b: number): [[number,number]] => {
+const pureHandler = (a: number, b: number): [[number, number]] => {
     console.log("a,b", a, b)
-    return [[a,b]]}
+    return [[a, b]]
+}
 
 // build the handler program, which sequences the input services to 
 // provide the params to the pureHandler, and the output services to 
@@ -35,27 +60,13 @@ const prog = handleEventProgram(
     pureHandler,
     [TestOutputServiceC])
 
-const a = Effect.provideService(
-    prog,
-    TestInputServiceA,
-    TestInputServiceA.of({
-        fx: () => Effect.succeed(10)}))
+Deno.test("test service builder", () => {
+    
+    // provide Service impls
+    const runnable = Effect.provide(prog, context)
 
-const b = Effect.provideService(
-    a,
-    TestInputServiceB,
-    TestInputServiceB.of({
-        fx: () => Effect.succeed(5)}))
+    // run the program
+    const r = Effect.runSync(runnable)
 
-const c = Effect.provideService(
-    b,
-    TestOutputServiceC,
-    TestOutputServiceC.of( {
-        fx: (v?: [number,number]) => {
-            console.log("[number,number]", v)
-            return Effect.succeed(v===undefined ? [0,0] :v)}}))
-
-Deno.test("test service builder", ()=> {
-    const r = Effect.runSync(c)
-
-    assertEquals(r, [[10,5]])})
+    assertEquals(r, [[10, 5]])
+})
