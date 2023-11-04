ce1f76c88d87d2e41c8fc32b158d2fb140c15a14
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-11-04 17:23:02 +0000

    allowing multiple fns per service interface

---
+++ refine_map_multi_service.ts
@@ -0,0 +1,178 @@
+// deno-lint-ignore-file no-explicit-any
+import { Effect, Context } from "effect"
+
+// the weakness of the previous refine_map approach is that each 
+// service has but a single fx function, which will lead to 
+// a proliferation of services and feels unnatural
+
+// if instead a service could have a number of effectful functions,
+// each taking a single arg (for simplicity, at least in the first impl)
+// and return an Effect ... could we specify the function with an
+// as-const string in the StepSpec, then infer everything else we 
+// need - R,E,D,V ?
+
+// reference:
+// https://dev.to/ecyrbe/how-to-use-advanced-typescript-to-define-a-pipe-function-381h
+
+// each Effectful step defines:
+// - a key
+// - a tag for an FxService interface
+// - a key for an FxServiceFn on the FxService
+// - a fn which takes the output map of the previous step and
+//   returns the input type of the FxServiceFn
+// 
+// - and the output of the step is a new map type combining the inmap
+//   with the new key and output value from the FxServiceFn Effect
+
+// an FxService interface will have 1 or more FxService fns
+export interface FxServiceFn<D, R, E, V> {
+    (d: D): Effect.Effect<R, E, V>
+}
+
+export type FxServiceTag<I, S> = Context.Tag<I, S>
+export type ExtractTagId<T> = T extends FxServiceTag<infer I, infer _S> ? I : never
+export type ExtractTagService<T> = T extends FxServiceTag<infer _I, infer S> ? S : never
+
+// data defining a single Effectful step towards building an Object.
+// f transforms the Object-so-far:A into the argument D of 
+// the service function F on service interface S,
+// and the output of the service function will then be added to the Object-so-far
+// at {K: V}
+export type StepSpec<K extends string, A, D, I, S,
+    FK extends keyof ExtractTagService<FxServiceTag<I, S>>,
+    // hmm... maybe i want the service fn type here, even though it's derived... then i can provide
+    // a calculated fn from pipeline and if it doesn't meet the constraint, desirable boom!
+    F extends ExtractTagService<FxServiceTag<I, S>>[FK] = ExtractTagService<FxServiceTag<I, S>>[FK],
+> = {
+    // the key at which the service output will be added to the pipeline accumulator object A
+    readonly k: K
+    // a pure function which maps the accumulator to the service input D
+    readonly f: (arg: A) => D
+    // a service interface and the key of a service fn
+    readonly svc: FxServiceTag<I, S>
+    readonly svcFn: FK
+}
+
+// extract the FxServiceTag from a StepSpec
+export type ExtractFxServiceTag<T> = T extends StepSpec<infer _K, infer _A, infer _D, infer _I, infer _S, infer _FK>
+    ? T["svc"]
+    : never
+
+// extract the service Fn from a StepSpec
+export type ExtractFxServiceFn<T> = T extends StepSpec<infer _K, infer _A, infer _D, infer _I, infer _S, infer FK>
+    ? ExtractTagService<T["svc"]>[FK]
+    : never
+
+// recursively infer a tuple-type for an Effectful Object builder pipeline
+// from a tuple of StepSpecs, building up the Obj type along the way
+export type ObjectPipeline<Specs extends readonly [...any[]],
+    ObjAcc,
+    StepAcc extends [...any[]] = []> =
+
+    // case: final spec - deliver final pipeline tuple type from StepAcc
+    Specs extends readonly [infer Head]
+    ? Head extends StepSpec<infer K, infer _A, infer D, infer I, infer S, infer FK>
+    ? ExtractFxServiceFn<Head> extends FxServiceFn<D, infer _R, infer _E, infer _V>
+    // the final pipeline
+    ? readonly [...StepAcc, StepSpec<K, ObjAcc, D, I, S, FK, ExtractFxServiceFn<Head>>]
+
+    // case: there are more specs - add to StepAcc and ObjAcc and recurse
+    : Specs extends readonly [infer Head, ...infer Tail]
+    ? Head extends StepSpec<infer HK, infer _HA, infer HD, infer HI, infer HS, infer HFK>
+    ? ExtractFxServiceFn<Head> extends FxServiceFn<HD, infer _HR, infer _HE, infer HV>
+    ? Tail extends readonly [infer Next, ...any]
+    ? Next extends StepSpec<infer _NK, infer _NA, infer _ND, infer _NI, infer _NS, infer _NFK>
+    ? ObjectPipeline<Tail,
+        ObjAcc & { [K in HK]: HV },
+        [...StepAcc, StepSpec<HK, ObjAcc, HD, HI, HS, HFK>]>
+    : ["ObjectPipelineFail", "G", Specs] // Next extends StepSpec
+    : ["ObjectPipelineFail", "F", Specs] // Tail extends [infer Next, ...any]
+    : ["ObjectPipelineFail", "E", Specs] // ExtractFxServiceFn<Head> extends FxServiceFn
+    : ["ObjectPipelineFail", "D", Specs] // Head extends StepSpec
+    : ["ObjectPipelineFail", "C", Specs] // ExtractFxServiceFn<Head> extends FxServiceFn
+    : ["ObjectPipelineFail", "B", Specs] // Head extends StepSpec
+    : ["ObjectPipelineFail", "A", Specs] // Specs extends [infer Head]
+
+// builds a new Object type from an intersected ObjAcc type,
+// which makes the intellisense much simpler
+// https://stackoverflow.com/questions/57683303/how-can-i-see-the-full-expanded-contract-of-a-typescript-type
+export type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+// get the final Object type from a list of StepSpecs
+export type FinalObjectType<Specs extends readonly [...any[]], ObjAcc> =
+    ObjectPipeline<Specs, ObjAcc> extends readonly [...infer _Prev, infer Last]
+    ? Last extends StepSpec<infer LK, infer LA, infer LD, infer _LI, infer _LS, infer _LFK>
+    ? ExtractFxServiceFn<Last> extends FxServiceFn<LD, infer _LR, infer _LE, infer LV>
+    ? Expand<LA & { [K in LK]: LV }>
+    : ["FinalObjectTypeFail", "C", ObjectPipeline<Specs, ObjAcc>] // ExtractFxServiceFn<Last> extends FxServiceFn
+    : ["FinalObjectTypeFail", "B", ObjectPipeline<Specs, ObjAcc>] // Last
+    : ["FinalObjectTypeFail", "A", ObjectPipeline<Specs, ObjAcc>] // ObjectPipeline
+
+// since we want to pass an initial Data type param, but to infer 
+// StepSpecs - and typescript inference is all-or-nothing, we must curry
+// https://effectivetypescript.com/2020/12/04/gentips-1-curry/
+export declare function buildObjectPipelineProg<Init>():
+    <StepSpecs extends readonly [...any[]]>
+
+        // and this trick allows the StepSpecs param to be typed as
+        //   readonly[...StepSpecs]
+        // while also applying the ObjectPipeline type checks
+        (_stepSpecs: ObjectPipeline<StepSpecs, Init> extends readonly [...StepSpecs] ? readonly [...StepSpecs] : ObjectPipeline<StepSpecs, Init>)
+
+        => (arg: Init) => Effect.Effect<never, never, FinalObjectType<StepSpecs, Init>>
+
+//////////////////////////////////////////////////////////////////////////////
+
+// now to demonstrate...
+
+// first some services...
+
+export type Org = {
+    id: string
+    name: string
+}
+interface OrgService { readonly _: unique symbol }
+export interface OrgServiceI {
+    readonly get: (nick: string) => Effect.Effect<never, never, Org>
+}
+export const OrgService = Context.Tag<OrgService, OrgServiceI>("OrgService")
+
+export type User = {
+    id: string
+    name: string
+}
+interface UserService { readonly _: unique symbol }
+// the service interface
+export interface UserServiceI {
+    readonly get: (d: { org_id: string, user_id: string }) => Effect.Effect<never, never, User>
+}
+export const UserService = Context.Tag<UserService, UserServiceI>("UserService")
+
+// then some computation steps...
+
+// as const is required to prevent the k from being widened to a string type
+// and to ensure the specs array is interpreted as a tuple
+const getOrgStepSpec: StepSpec<"org", {data: {org_nick: string}}, string, OrgService, OrgServiceI, "get"> =
+{
+    k: "org" as const,
+    f: (d: { data: { org_nick: string } }) => d.data.org_nick,
+    svc: OrgService,
+    svcFn: "get" as const
+}
+const getUserStepSpec: StepSpec<"user", { data: { user_id: string }, org: Org }, { org_id: string, user_id: string }, UserService, UserServiceI, "get"> =
+{
+    k: "user" as const,
+    // note that this fn depends on the output of a getOrgStep
+    f: (d: { data: { user_id: string }, org: Org }) => { return { org_id: d.org.id, user_id: d.data.user_id } },
+    svc: UserService,
+    svcFn: "get" as const
+}
+export const specs = [
+    getOrgStepSpec,
+    getUserStepSpec
+] as const
+
+// and finally, to the object pipeline program... 
+// each step's f is checked against the accumulated object from the previous steps
+
+export const prog = buildObjectPipelineProg<{ data: { org_nick: string, user_id: string } }>()(specs)
