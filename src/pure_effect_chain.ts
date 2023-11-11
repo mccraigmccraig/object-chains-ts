b8d9248b7f249bd4d171a2d9fb05acdbae53f8e1
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-11-11 08:30:34 +0000

    skeleton of pureFnChain
    need a better name

---
+++ src/pure_effect_chain.ts
@@ -0,0 +1,71 @@
+// deno-lint-ignore-file no-explicit-any
+import { Effect, Context } from "effect"
+import { ObjectStepsInputTuple, ObjectStepsValueTuple, ObjectStepsDeps, ObjectStepsErrors, ChainObjectStepsReturn, chainObjectStepsProg, tupleMapObjectStepsProg } from "./object_builders.ts"
+
+// business logic is encapsulated in a pure function
+//   (in: extends Object) => [...(extends Object)[]]
+// a (chain of) effectful input steps build the in: Object, and 
+// a (chain of) effectful output steps process the output tuple
+
+// so chain has:
+// [(Input) => Effect<R,E,PureInput>,
+//  (PureInput) => PureOutput,
+//  (PureOutput) => Effect<R,E,Output>]
+
+// but that's too abstract ... we are going to use the object_builders for
+// the input and output chains...
+//
+// Input is an Object with a tag: ,
+// input steps extend the Input Object
+// the PureInput is the accumulation of the Input with all the input steps
+// the tag: is used to associate the PureOutput on the Object
+// output steps extend the Input Object
+// the result is the accumulation of the PureInput with the PureOutput and
+// all the output steps
+
+export interface Tagged { readonly tag: string }
+export type Tag<T extends Tagged> = { readonly tag: T['tag'] }
+
+// build a tag value for an Tagged type,
+// forcing the tag param to match the Tagged.tag string
+// const aTag = tag<ATagged>("ATagged")
+export const tag = <T extends Tagged>(tag: T['tag']): Tag<T> => { return { tag } }
+
+// maybe we have different sorts of steps
+// - input service-fn steps - chain steps
+// - pure-step - in:obj -> out:tuple
+// - output service-fn steps - tuple-map steps
+
+export function makePureFnChain<I extends Tagged>() {
+    return function <InputStepSpecs extends readonly [...any[]],
+        OutputStepSpecs extends readonly [...any[]]>
+        (inputStepSpecs: InputStepSpecs,
+            pureFn: (pi: ChainObjectStepsReturn<InputStepSpecs, I>) => ObjectStepsInputTuple<OutputStepSpecs>,
+            outputStepSpecs: OutputStepSpecs) {
+        
+        console.log("CREATE PURE_FN_CHAIN", inputStepSpecs, pureFn, outputStepSpecs)
+        const inputChainProg = chainObjectStepsProg<I>()(inputStepSpecs)
+        const outputStepsProg = tupleMapObjectStepsProg<ChainObjectStepsReturn<InputStepSpecs, I> & { [_K in I['tag']]: ObjectStepsInputTuple<OutputStepSpecs> }>()(outputStepSpecs)
+
+        const r = (i: I) => {
+            console.log("RUN PURE_FN_CHAIN: i", i)
+            return Effect.gen(function* (_) {
+                const pi: any = yield* _(inputChainProg(i))
+                console.log("RUN PURE_FN_CHAIN: pi", pi)
+                const po = pureFn(pi)
+                console.log("RUN PURE_FN_CHAIN: po", po)
+                const oi = { ...pi, [i.tag]: po }
+                console.log("RUN PURE_FN_CHAIN: oi", oi)
+                const oo = yield* _(outputStepsProg(oi))
+                console.log("RUN PURE_FN_CHAIN: oo", oo)
+                const v = { ...oi, ...oo }
+                console.log("RUN PURE_FN_CHAIN: v", v)
+                return v
+            })
+        }
+
+        return r as (i: I) => Effect.Effect<ObjectStepsDeps<InputStepSpecs> | ObjectStepsDeps<OutputStepSpecs>,
+            ObjectStepsErrors<InputStepSpecs> | ObjectStepsErrors<OutputStepSpecs>,
+            ObjectStepsValueTuple<OutputStepSpecs>>
+    }
+}
---
+++ src/pure_effect_chain_test.ts
@@ -0,0 +1,102 @@
+import { assertEquals } from "assert"
+import { Effect, Context } from "effect"
+import { invokeFxServiceFn } from "./fx_service_fn.ts"
+import { makePureFnChain } from "./pure_effect_chain.ts"
+
+export type Org = {
+    id: string
+    name: string
+}
+interface OrgService { readonly _: unique symbol }
+export interface OrgServiceI {
+    readonly getById: (id: string) => Effect.Effect<never, never, Org>
+    readonly getByNick: (nick: string) => Effect.Effect<never, never, Org>
+}
+export const OrgService = Context.Tag<OrgService, OrgServiceI>("OrgService")
+
+// $ExpectType FxServiceFn<string, OrgService, never, Org>
+export const getOrgByNick = invokeFxServiceFn(OrgService, "getByNick")
+
+export type User = {
+    id: string
+    name: string
+}
+interface UserService { readonly _: unique symbol }
+// the service interface
+export interface UserServiceI {
+    readonly getByIds: (d: { org_id: string, user_id: string }) => Effect.Effect<never, never, User>
+}
+export const UserService = Context.Tag<UserService, UserServiceI>("UserService")
+
+// $ExpectType FxServiceFn<{org_id: string, user_id: string}, UserService, never, User>
+export const getUserByIds = invokeFxServiceFn(UserService, "getByIds")
+
+interface PushNotificationService { readonly _: unique symbol }
+export interface PushNotificationServiceI {
+    readonly sendPush: (d: {user_id: string, message: string}) => Effect.Effect<never, never, string>
+}
+export const PushNotificationService = Context.Tag<PushNotificationService, PushNotificationServiceI>("PushNotificationService")
+
+export const sendPush = invokeFxServiceFn(PushNotificationService, "sendPush")
+
+//////////////////////////////////////////////////////////////////////////////
+
+// then some computation steps...
+
+// as const is required to prevent the k from being widened to a string type
+// and to ensure the specs array is interpreted as a tuple
+const getOrgObjectStepSpec /* : ObjectStepSpec<"org", { data: { org_nick: string } }, string, OrgService, never, Org> */ =
+{
+    k: "org" as const,
+    inFn: (d: { data: { org_nick: string } }) => d.data.org_nick,
+    svcFn: getOrgByNick
+}
+const getUserObjectStepSpec /* : ObjectStepSpec<"user", { data: { user_id: string }, org: Org }, {org_id: string, user_id: string}, UserService, never, User> */ =
+{
+    k: "user" as const,
+    // note that this fn depends on the output of an OrgServiceI.getBy* step
+    inFn: (d: { data: { user_id: string }, org: Org }) => { return { org_id: d.org.id, user_id: d.data.user_id } },
+    svcFn: getUserByIds
+}
+const pureSendWelcomePush = (org: Org, user: User) => {
+    return [{user_id: user.id, message: "Welcome " + user.name + " of " + org.name}]
+}
+
+const sendPusnNotificationStepSpec =
+{
+    k: "sendPush" as const,
+    inFn: (x:any) => x,
+    svcFn: sendPush    
+}
+
+// a simple context with an OrgService and a UserService which echo data back
+const echoContext = Context.empty().pipe(
+    Context.add(OrgService, OrgService.of({
+        getById: (id: string) => Effect.succeed({ id: id, name: "Foo" }),
+        getByNick: (nick: string) => Effect.succeed({ id: nick, name: "Foo" })
+    })),
+    Context.add(UserService, UserService.of({
+        getByIds: (d: { org_id: string, user_id: string }) => Effect.succeed({ id: d.user_id, name: "Bar" })
+    })),
+    Context.add(PushNotificationService, PushNotificationService.of({
+        sendPush: (d: {user_id: string, message: string}) => Effect.succeed("push sent OK: " + d.message)
+    })))
+
+
+Deno.test("pure effect chain", () => {
+    type INPUT = { tag: "INPUT", data: { org_nick: string, user_id: string } }
+    const input: INPUT = { tag: "INPUT", data: { org_nick: "foo", user_id: "100" } }
+
+    const pureChainEffect = makePureFnChain<INPUT>()(
+        [getOrgObjectStepSpec, getUserObjectStepSpec],
+        pureSendWelcomePush,
+        [sendPusnNotificationStepSpec])(input)
+    
+    const prog = Effect.provide(pureChainEffect, echoContext)
+    const r = Effect.runSync(prog)
+
+    assertEquals(r, {
+        org: { id: "foo", name: "Foo" },
+        user: { id: "100", name: "Bar" }
+    })
+})
