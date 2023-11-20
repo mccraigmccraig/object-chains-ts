e9d58e552a36fe23d1fbd2a4c919dc1f31121e3c
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-11-20 11:10:46 +0000

    fx_fx_test text width

--- src/fx_fn_test.ts
+++ src/fx_fn_test.ts
@@ -5,8 +5,10 @@ import { OrgService, getOrgByNick } from "./test_services.ts"
 
 const context = Context.empty().pipe(
     Context.add(OrgService, OrgService.of({
-        getById: (id: string) => Effect.succeed({ id: id, name: id.toString() }),
-        getByNick: (nick: string) => Effect.succeed({ id: nick, name: nick })
+        getById: (id: string) =>
+            Effect.succeed({ id: id, name: id.toString() }),
+        getByNick: (nick: string) =>
+            Effect.succeed({ id: nick, name: nick })
     })))
 
 const prog = Effect.gen(function* (_) {
@@ -14,7 +16,8 @@ const prog = Effect.gen(function* (_) {
     return org;
 })
 
-Deno.test("invokeServiceFxFn retrieves the service and invokes the FxFn", () => {
+Deno.test("invokeServiceFxFn retrieves the service and invokes the FxFn",
+    () => {
 
     const runnable = Effect.provide(prog, context)
 
