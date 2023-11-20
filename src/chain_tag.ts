df5a788ee9e449df4d49277960772466925240eb
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-11-20 10:27:19 +0000

    chain_tag text width

--- src/chain_tag.ts
+++ src/chain_tag.ts
@@ -12,13 +12,17 @@ export type ChainTag<T extends ChainTagged> = {
 // build a tag value for a Tagged type,
 // forcing the tag param to match the Tagged.tag string
 // const aTag = tag<ATagged>("ATagged")
-export const chainTag = <T extends ChainTagged>(chainTagStr: T['_tag']): ChainTag<T> => {
+export const chainTag =
+    <T extends ChainTagged>(chainTagStr: T['_tag']): ChainTag<T> => {
     return {
         [chainTagKey]: chainTagStr
     }
 }
 
 // get the tag string from a Tag
-export const chainTagStr = <T extends ChainTagged>(chainTag: ChainTag<T>): ChainTag<T>[typeof chainTagKey] => {
+export const chainTagStr =
+    <T extends ChainTagged>(chainTag: ChainTag<T>)
+        : ChainTag<T>[typeof chainTagKey] => {
+        
     return chainTag[chainTagKey]
 }
