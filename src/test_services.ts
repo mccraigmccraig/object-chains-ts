9a22c97514139c4307194d863922f1285d5e33e0
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-11-20 11:14:43 +0000

    test_services text width

--- src/test_services.ts
+++ src/test_services.ts
@@ -28,10 +28,13 @@ export type User = {
 type UserService = { readonly _: unique symbol }
 // the service interface
 export interface UserServiceI {
-    readonly getByIds: (d: { org_id: string, user_id: string }) => Effect.Effect<never, never, User>
-    readonly change: (d: {old: User, new: User}) => Effect.Effect<never, never, User>
+    readonly getByIds: (d: { org_id: string, user_id: string }) =>
+        Effect.Effect<never, never, User>
+    readonly change: (d: { old: User, new: User }) =>
+        Effect.Effect<never, never, User>
 }
-export const UserService = Context.Tag<UserService, UserServiceI>("UserService")
+export const UserService =
+    Context.Tag<UserService, UserServiceI>("UserService")
 
 // $ExpectType FxFn<{org_id: string, user_id: string}, UserService, never, User>
 export const getUserByIds = invokeServiceFxFn(UserService, "getByIds")
@@ -42,9 +45,12 @@ export const changeUser = invokeServiceFxFn(UserService, "change")
 
 type PushNotificationService = { readonly _: unique symbol }
 export interface PushNotificationServiceI {
-    readonly sendPush: (d: { user_id: string, message: string }) => Effect.Effect<never, never, string>
+    readonly sendPush: (d: { user_id: string, message: string }) =>
+        Effect.Effect<never, never, string>
 }
-export const PushNotificationService = Context.Tag<PushNotificationService, PushNotificationServiceI>("PushNotificationService")
+export const PushNotificationService =
+    Context.Tag<PushNotificationService,
+        PushNotificationServiceI>("PushNotificationService")
 
 export const sendPush = invokeServiceFxFn(PushNotificationService, "sendPush")
 
@@ -56,9 +62,11 @@ export const testServiceContext = Context.empty().pipe(
         getByNick: (nick: string) => Effect.succeed({ id: nick, name: "Foo" })
     })),
     Context.add(UserService, UserService.of({
-        getByIds: (d: { org_id: string, user_id: string }) => Effect.succeed({ id: d.user_id, name: "Bar" }),
-        change: (d: {old: User, new: User}) => Effect.succeed(d.new)
+        getByIds: (d: { org_id: string, user_id: string }) =>
+            Effect.succeed({ id: d.user_id, name: "Bar" }),
+        change: (d: { old: User, new: User }) => Effect.succeed(d.new)
     })),
     Context.add(PushNotificationService, PushNotificationService.of({
-        sendPush: (d: { user_id: string, message: string }) => Effect.succeed("push sent OK: " + d.message)
+        sendPush: (d: { user_id: string, message: string }) =>
+            Effect.succeed("push sent OK: " + d.message)
     })))
