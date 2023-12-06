fdba9c7c423c13161a69175d4662ef9a78671504
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-12-06 11:00:52 +0000

    add a cons append fn

--- src/cons.ts
+++ src/cons.ts
@@ -5,8 +5,8 @@ const NoneTag: unique symbol = Symbol("ConsNone")
 export type None = { readonly _tag: typeof NoneTag }
 export const None: None = { _tag: NoneTag }
 
-// it's probably a cons, but we're avoiding recursive types because 
-// they seem to make the compiler choke
+// it's probably a cons, but we're only checking
+// one level with this type
 // deno-lint-ignore no-explicit-any
 export type NRCons<T> = None | readonly [T, any]
 
@@ -16,7 +16,7 @@ export type UPCons = None | readonly [any, any]
 
 // a Cons type with limited conditional recursion
 // T is the base type for the values
-export type Cons<T, C = None> =
+export type Cons<T, C> =
     C extends None
     ? None
     : C extends readonly [infer H extends T, infer R]
@@ -35,7 +35,7 @@ export function cons<T>() {
     }
 }
 
-type First<T, C> =
+export type First<T, C> =
     C extends Cons<T, C>
     ? C extends None
     ? None
@@ -58,7 +58,7 @@ export function first<T>() {
     }
 }
 
-type Rest<T, C> =
+export type Rest<T, C> =
     C extends Cons<T, C>
     ? C extends None
     ? None
@@ -81,7 +81,33 @@ export function rest<T>() {
     }
 }
 
-type Reverse<T, C, Acc = None> = 
+export type Last<T, C> =
+    C extends None
+    ? None
+    : C extends readonly [infer F, None]
+    ? F
+    : C extends readonly [infer _F extends T, infer R]
+    ? Last<T, R>
+    : never
+
+export function last<T>() {
+    return function <const C>(c: C extends Cons<T, C> ? C : Cons<T, C>)
+        : Last<T, C> {
+
+        let result: Cons<T, None> = None
+        let cursor = c
+        while (!('_tag' in cursor)) {
+            // deno-lint-ignore no-explicit-any
+            result = cursor[0] as any
+            cursor = cursor[1]
+        }
+
+        return result as Last<T, C>
+        
+    }
+}
+
+export type Reverse<T, C, Acc = None> = 
     C extends Cons<T, C> 
     ? C extends None 
     ? Acc 
@@ -94,7 +120,7 @@ export function reverse<T>() {
     return function <const C>(c: C extends Cons<T, C> ? C : Cons<T, C>)
         : Reverse<T, C> {
         
-        let result: Cons<T> = None
+        let result: Cons<T, None> = None
         let cursor = c 
         while (!('_tag' in cursor)) {
             // deno-lint-ignore no-explicit-any
@@ -106,10 +132,34 @@ export function reverse<T>() {
     }
 }
 
+export type Append<T, C, V extends T> =
+    C extends None
+    ? readonly [V, None]
+    : C extends readonly [infer F extends T, infer R]
+    ? readonly [F, Append<T, R, V>]
+    : never
+
+// append an element to a cons list - 
+// NB: builds an entirely new cons list
+export function append<T>() {
+    return function <const C, V extends T>(
+        c: C extends Cons<T, C> ? C : Cons<T, C>,
+        v: V): Append<T, C, V> {
+
+        // deno-lint-ignore no-explicit-any
+        const reversed = reverse<T>()(c) as any
+        const prepended = cons<T>()(v, reversed)
+        return reverse<T>()(prepended) as Append<T, C, V>
+    }
+}
+
 // itereatively build a list
 export const a = cons<number>()(2, None)
 export const b = cons<number>()(1, a)
 export const c = cons<number>()(0, b)
+
+declare const f: readonly [number, None]
+export const g = cons<number>()(10, f)
 
 // get some first elements
 export const hn = first<number>()(None)
@@ -117,7 +167,14 @@ export const ha = first<number>()(a)
 export const hb = first<number>()(b)
 export const hc = first<number>()(c)
 
+// get some last elements
+export const ln = last<number>()(None)
+export const la = last<number>()(a)
+export const lb = last<number>()(b)
+export const lc = last<number>()(c)
+
 // get some rests
+export const rn = rest()(None)
 export const ra = rest()(a)
 export const rb = rest()(b)
 export const rc = rest()(c)
@@ -127,3 +184,9 @@ export const revn = reverse<number>()(None)
 export const reva = reverse<number>()(a)
 export const revb = reverse<number>()(b)
 export const revc = reverse<number>()(c)
+
+// append to a list 
+export const appn = append<number>()(None, 100)
+export const appa = append<number>()(a, 100)
+export const appb = append<number>()(b, 100)
+export const appc = append<number>()(c, 100)
